053f48998467947489416a62b377ce8a21fc79e0
daochun <>
2019-11-30 17:39:33 +0800

    安装babel

--- design-pattern-test/src/index.js
+++ design-pattern-test/src/index.js
@@ -1,1 +1,11 @@
-alert(100)
+class Person {
+    constructor(name) {
+        this.name = name
+    }
+    getName() {
+        return this.name
+    }
+}
+
+let p = new Person("张三")
+alert(p.getName())
--- design-pattern-test/webpack.dev.config.js
+++ design-pattern-test/webpack.dev.config.js
@@ -1,7 +1,27 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
     entry: './src/index.js',
     output: {
         path: __dirname,
-        filename: './release/bundle.js' 
+        filename: './release/bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.js?$/,
+            exclude: /(node_modules)/,
+            loader: 'babel-loader'
+        }]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        })
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, './release'), // html页面所在的相对目录
+        open: true,  // 自动打开 浏览器
+        port: 9000   // 端口
     }
 }
